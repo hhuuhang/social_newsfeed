69dcdcc4e863c18e4207235129e9bb6771f59a49
HangHH <hhuuhang@gmail.com>
2022-07-26 00:56:28 +0700

    Welcome Screen completed

--- lib/modules/01_welcome_page/page/welcome_page.dart
+++ lib/modules/01_welcome_page/page/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:social_newsfeed/modules/common_widget/widgets/buttons/primary_button.dart';
 import 'package:social_newsfeed/modules/common_widget/widgets/buttons/white_button.dart';
+import 'package:social_newsfeed/themes/app_fonts.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -10,94 +11,101 @@ class WelcomePage extends StatelessWidget {
     final Size size = MediaQuery.of(context).size;
     final double maxWidthScreen = size.width;
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: maxWidthScreen,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/background_image.png'),
-              fit: BoxFit.cover,
-            ),
+      body: Container(
+        width: maxWidthScreen,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background_image.png'),
+            fit: BoxFit.cover,
           ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Column(
-              children: <Widget>[
-                const Spacer(
-                  flex: 8,
-                ),
-                const Text(
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30),
+          child: Column(
+            children: <Widget>[
+              const Spacer(
+                flex: 8,
+              ),
+              SizedBox(
+                width: maxWidthScreen - 60,
+                child: const Text(
                   'Find new\nfriends nearby',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w900,
+                    fontFamily: AppFonts.avenir,
                   ),
                 ),
-                const Spacer(
-                  flex: 1,
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              const Text(
+                'With milions of users all over the world, we gives you the ability to connect with people no matter where you are.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontFamily: AppFonts.avenir,
                 ),
-                const Text(
-                  'With milions of users all over the world, we gives you the ability to connect with people no matter where you are.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                  ),
-                ),
-                const Spacer(
-                  flex: 1,
-                ),
-                WhiteButton(
-                  content: 'Login',
-                  onClick: () {},
-                ),
-                const Spacer(
-                  flex: 1,
-                ),
-                PrimaryButton(
-                  content: 'Login',
-                  onClick: () {},
-                ),
-                const Spacer(
-                  flex: 1,
-                ),
-                const Text(
-                  'Or lof in with',
-                  style: TextStyle(color: Colors.white),
-                ),
-                SizedBox(
-                  width: 120,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        height: 30,
-                        width: 30,
-                        decoration: const BoxDecoration(
-                          color: Colors.amber,
-                          image: DecorationImage(
-                            image: AssetImage('assets/icons/facebook.png'),
-                          ),
-                        ),
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              WhiteButton(
+                content: 'Login',
+                onClick: () {},
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              PrimaryButton(
+                content: 'Sign Up',
+                onClick: () {},
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              const Text(
+                'Or log in with',
+                style: TextStyle(color: Colors.white),
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+              SizedBox(
+                width: 150,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 30,
+                      child: InkWell(
+                        child: Image.asset('assets/icons/facebook.png'),
+                        onTap: () {},
                       ),
-                      Container(
-                        height: 30,
-                        width: 30,
-                        color: Colors.blue,
+                    ),
+                    SizedBox(
+                      width: 30,
+                      child: InkWell(
+                        child: Image.asset('assets/icons/twitter.png'),
+                        onTap: () {},
                       ),
-                      Container(
-                        height: 30,
-                        width: 30,
-                        color: Colors.amber,
+                    ),
+                    SizedBox(
+                      width: 30,
+                      child: InkWell(
+                        child: Image.asset('assets/icons/google.png'),
+                        onTap: () {},
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                const Spacer(
-                  flex: 1,
-                ),
-              ],
-            ),
+              ),
+              const Spacer(
+                flex: 1,
+              ),
+            ],
           ),
         ),
       ),
