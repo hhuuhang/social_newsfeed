289de0ec34a79b38a72ed94b9b60d5ab00679645
HangHH <hhuuhang@gmail.com>
2022-07-31 21:12:23 +0700

    done login UI

--- lib/modules/02_login/page/login_page.dart
+++ lib/modules/02_login/page/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:social_newsfeed/modules/02_login/widgets/text_field_custom.dart';
+import 'package:social_newsfeed/modules/common_widget/widgets/buttons/primary_button.dart';
 import 'package:social_newsfeed/themes/app_fonts.dart';
 
 class LoginPage extends StatelessWidget {
@@ -10,61 +11,112 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/loginbackground.png'),
-              fit: BoxFit.cover,
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/loginbackground.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color.fromARGB(72, 36, 42, 55),
+                      Color(0xff242A37)
+                    ]),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Icon(Icons.arrow_back),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Welcome back",
+                          style: TextStyle(
+                            fontFamily: AppFonts.avenir,
+                            fontSize: 40,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const Text(
+                          "Login to your account",
+                          style: TextStyle(
+                            fontFamily: AppFonts.avenir,
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        CustomTextField(
+                          hint: "Email",
+                          inputType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        CustomTextField(
+                          hint: "Password",
+                          inputType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        PrimaryButton(
+                          content: "LOGIN",
+                          onClick: () {},
+                        ),
+                        const SizedBox(height: 50),
+                        Center(
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "Forgot your password?",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: AppFonts.avenir,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      "Welcome back",
-                      style: TextStyle(
-                        fontFamily: AppFonts.avenir,
-                        fontSize: 40,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      "Login to your account",
-                      style: TextStyle(
-                        fontFamily: AppFonts.avenir,
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    TextField(),
-                    // CustomTextField(
-                    //   hint: "Email",
-                    // ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/modules/02_login/widgets/text_field_custom.dart
+++ lib/modules/02_login/widgets/text_field_custom.dart
@@ -4,13 +4,13 @@ import 'package:flutter/src/widgets/framework.dart';
 
 class CustomTextField extends StatefulWidget {
   final String hint;
-  final TextEditingController controller;
+  // final TextEditingController controller;
   final TextInputType inputType;
   final bool obscureText;
 
   CustomTextField({
     required this.hint,
-    required this.controller,
+    // required this.controller,
     this.inputType = TextInputType.text,
     this.obscureText = false,
   });
@@ -23,54 +23,30 @@ class _CustomTextFieldState extends State<CustomTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10.0),
-      child: TextField(
-        obscureText: widget.obscureText,
-        //keyboardType: widget.inputType,
-        // controller: widget.controller,
-        decoration: InputDecoration(
-          // hintStyle: const TextStyle(),
-          border: InputBorder.none,
-          hintText: widget.hint,
+    return Opacity(
+      opacity: 0.3,
+      child: Container(
+        alignment: Alignment.center,
+        height: 44,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(185, 0, 0, 0),
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: TextField(
+            obscureText: widget.obscureText,
+            //keyboardType: widget.inputType,
+            // controller: widget.controller,
+            decoration: InputDecoration(
+              hintStyle: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold),
+              border: InputBorder.none,
+              hintText: widget.hint,
+            ),
+          ),
         ),
       ),
     );
   }
 }
-
-
-// class TextFieldCustom extends StatefulWidget {
-//   TextFieldCustom({required this.hint});
-//   final String hint;
-
-//   @override
-//   State<TextFieldCustom> createState() => _TextFieldCustomState();
-// }
-
-// class _TextFieldCustomState extends State<TextFieldCustom> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Opacity(
-//       opacity: 0.3,
-//       child: Container(
-//         alignment: Alignment.center,
-//         height: 44,
-//         decoration: BoxDecoration(
-//           color: Colors.white,
-//           borderRadius: BorderRadius.circular(24),
-//         ),
-//         child: const Padding(
-//           padding: EdgeInsets.only(left: 20, right: 20),
-//           child: TextField(
-//             decoration: InputDecoration(
-//                 border: InputBorder.none,
-//                 hintText: widget.hint,
-//                 hintStyle:
-//                     TextStyle(color: Colors.red, fontWeight: FontWeight.w900)),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
